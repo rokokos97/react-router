ca0998ed579c61824f5c22a11f2a6ad8dd689640
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-03 11:26:49 +0100

    import query-string in posts.jsx import lodash add pagination cropPost with lodash slice.take.value!!!

--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -3,21 +3,25 @@ import PropTypes from 'prop-types';
 import Post from './post';
 import PostsList from './postsList';
 import query from 'query-string';
+import _ from 'lodash';
 
 
 const Posts = ({match, location}) => {
   const search =query.parse(location.search);
-  console.log(search);
-
+  console.log(search.count);
   const postId = match.params.postId;
   const posts = [
     {id: 1, label: 'post 1'},
     {id: 2, label: 'post 2'},
     {id: 3, label: 'post 3'}];
+  const cropPosts = search.count?
+      _(posts).slice(0).take(search.count).value():
+      posts;
+  console.log(cropPosts);
   return <>
     {postId?
         <Post {...{posts}} id={postId}/>:
-        <PostsList {...{posts}}/>}
+        <PostsList posts={cropPosts}/>}
   </>;
 };
 
