31fe5e9dcc3d7827cf1b0f2b8e75e6008d2c15ce
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-03 13:30:37 +0100

    add inserted routing in dashboard.jsx

--- src/app/components/dashboard.jsx
+++ src/app/components/dashboard.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import {Link, Route, Switch} from 'react-router-dom';
+import Stats from './stats';
+import Edit from './edit';
 
 
 const Dashboard = () => {
-  return (<h2>Dashboard</h2>);
+  return (<div>
+    <h2>Dashboard</h2>
+    <ul>
+      <li><Link to={'/dashboard'}>Dashboards</Link></li>
+      <li> <Link to={'/dashboard/edit'}>Edit</Link></li>
+    </ul>
+    <Switch>
+      <Route path={'/dashboard/edit'} component={Edit}/>
+      <Route path={'/dashboard'} component={Stats}/>
+    </Switch>
+  </div>);
 };
 
 export default Dashboard;
---
+++ src/app/components/stats.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+const Stats = () => {
+  return (<h2>THIS IS FAKE STATISTIC PAGE</h2>);
+};
+export default Stats;
