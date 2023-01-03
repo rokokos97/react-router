0b736f18173060b3d8900787f8a6d0ee49eda7db
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-03 11:06:53 +0100

    import query-string in posts.jsx add pagination cropPost

--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Post from './post';
 import PostsList from './postsList';
+import query from 'query-string';
 
 
-const Posts = ({match}) => {
+const Posts = ({match, location}) => {
+  const search =query.parse(location.search);
+  console.log(search);
+
   const postId = match.params.postId;
-  console.log(postId);
   const posts = [
     {id: 1, label: 'post 1'},
     {id: 2, label: 'post 2'},
@@ -19,6 +22,7 @@ const Posts = ({match}) => {
 };
 
 Posts.propTypes = {
+  location: PropTypes.object,
   match: PropTypes.object.isRequired,
 };
 export default Posts;
--- src/app/components/postsList.jsx
+++ src/app/components/postsList.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const PostsList = ({posts}) => {
-  console.log(posts);
   return <>
     {
       posts.map((post)=><h3 key={post.id}>{post.label}</h3>)
